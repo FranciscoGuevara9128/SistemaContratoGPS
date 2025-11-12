de9bb9b19c25a14ea127a874a9a3a2e07828ac6b
Emma Serrano <emmaceciliaserranourroz@gmail.com>
2025-11-12 17:47:13 -0600

    Clase Vehiculo creada

---
+++ src/main/java/ni/edu/uam/SistemaContratoGPS/modelo/Vehiculo.java
@@ -0,0 +1,27 @@
+package ni.edu.uam.SistemaContratoGPS.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.Columns;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.Hidden;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="vehiculo")
+@Getter
+@Setter
+
+public class Vehiculo {
+    @Id
+    @Hidden
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="vehiculo_id", strategy = "uuid2")
+    private String vehiculo_id;
+
+    @Column(name="placa", length = 60, nullable = false)
+    private String placa;
+}
