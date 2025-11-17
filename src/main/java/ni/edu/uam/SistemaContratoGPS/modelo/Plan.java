96322c0c5574ffa62999d6f7b691b6459eae7f92
Emma Serrano <emmaceciliaserranourroz@gmail.com>
2025-11-17 10:35:42 -0600

    Se agregó enum Periodicidad y clase Plan. Se completó clase Vehículo.

---
+++ src/main/java/ni/edu/uam/SistemaContratoGPS/modelo/Plan.java
@@ -0,0 +1,37 @@
+package ni.edu.uam.SistemaContratoGPS.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.openxava.annotations.Hidden;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="planes")
+@Getter
+@Setter
+
+public class Plan {
+    @Id
+    @Hidden
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid2")
+    private String clienteId;
+
+    @Column(name="nombre", length = 30, nullable = false)
+    private String nombre;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="tipo_plan")
+    private Periodicidad tipoPlan;
+
+    @Column(name="precio_base", nullable = false)
+    private Double precioBase;
+
+    @Column(name="descripcion", length = 200)
+    private String descripcion;
+
+    @Column(name="activo", nullable = false, columnDefinition = "boolean default true")
+    private Boolean activo;
+}
--- src/main/java/ni/edu/uam/SistemaContratoGPS/modelo/Vehiculo.java
+++ src/main/java/ni/edu/uam/SistemaContratoGPS/modelo/Vehiculo.java
@@ -7,6 +7,7 @@ import org.hibernate.annotations.GenericGenerator;
 import org.openxava.annotations.Hidden;
 
 import javax.persistence.*;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
@@ -20,8 +21,31 @@ public class Vehiculo {
     @Hidden
     @GeneratedValue(generator = "system-uuid")
     @GenericGenerator(name = "system-uuid", strategy = "uuid2")
-    private String vehiculo_id;
+    private String vehiculoId;
 
-    @Column(name="placa", length = 60, nullable = false)
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="cliente_id")
+    private Cliente cliente;
+
+    @Column(name="placa", length = 60, nullable = false, unique = true)
     private String placa;
+
+    @Column(name="vin", length = 30, nullable = false, unique = true)
+    private String vin;
+
+    @Column(name="marca", length = 30, nullable = false)
+    private String marca;
+
+    @Column(name="modelo", length = 30, nullable = false)
+    private String modelo;
+
+    @Column(name="anio", nullable = false)
+    @Min(message="El año no puede ser menor a 1900", value=1900)
+    private Integer anio;
+
+    @Column(name="color", length = 30, nullable = false)
+    private String color;
+
+    @Column(name="activo", nullable = false, columnDefinition = "boolean default true")
+    private Boolean activo;
 }
